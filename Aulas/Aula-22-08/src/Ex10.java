e13c7c6386d38149cb974ab001cee3e6c4d48808
Letícia Pinheiro <leticiapinheiro3137@outlook.com>
2023-08-26 10:51:59 -0300

    att

---
+++ Aulas/Aula-22-08/src/Ex10.java
@@ -0,0 +1,17 @@
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class Ex10 {
+    public static void main (String[] args) throws IOException{
+        OutputStream os = new FileOutputStream("saida.txt");
+        OutputStreamWriter osw = new OutputStreamWriter(os);
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        bw.write("Java");
+        bw.newLine();
+        bw.close();
+    }
+}
